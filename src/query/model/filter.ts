0054f9775a69222f641366de9f8f5de4d29897dc
Sandro Schmid <saseb.schmid@gmail.com>
2017-11-30 14:54:26 +0100

    simplify `Filter`

--- src/query/model/filter.ts
+++ src/query/model/filter.ts
@@ -3,14 +3,10 @@ import { Predicate } from '../../model/predicate';
 export class Filter<DbItem> {
 
   constructor(private readonly _predicate: Predicate<DbItem>,
-              private readonly _requiresNormalization?: boolean) {
+              public readonly requiresNormalization: boolean = false) {
   }
 
   public test(item: DbItem): boolean {
     return this._predicate(item);
-  }
-
-  public requiresNormalization(): boolean {
-    return this._requiresNormalization;
   }
 }
