034b007efbaf0dc19fb415b72956586dc753d6d3
Sandro Schmid <saseb.schmid@gmail.com>
2018-01-16 18:38:57 +0100

    fix `addKeysToParentsHelper` (add `TypeMismatchError`s)

--- src/command/idb-command/idb-base-write-command.ts
+++ src/command/idb-command/idb-base-write-command.ts
@@ -1,4 +1,4 @@
-import { isNull, NdbDocument, NotFoundError, ValidKey } from '@normalized-db/core';
+import { isNull, NdbDocument, NotFoundError, TypeMismatchError, ValidKey } from '@normalized-db/core';
 import { Cursor, ObjectStore, Transaction } from 'idb';
 import { IdbContext } from '../../context/idb-context/idb-context';
 import { CreatedEvent } from '../../event/created-event';
@@ -124,6 +124,10 @@ export abstract class IdbBaseWriteCommand<T extends NdbDocument> extends IdbBase
   }
 
   private async addToParents(transaction: Transaction, parent: Parent | Parent[], keys: ValidKey[]): Promise<void> {
+    if (keys && keys.length === 0) {
+      return;
+    }
+
     if (Array.isArray(parent)) {
       await Promise.all(parent.map(p => this.addToParent(transaction, p, keys)));
     } else {
@@ -132,10 +136,6 @@ export abstract class IdbBaseWriteCommand<T extends NdbDocument> extends IdbBase
   }
 
   private async addToParent(transaction: Transaction, parent: Parent, keys: ValidKey[]): Promise<void> {
-    if (keys && keys.length === 0) {
-      return;
-    }
-
     const parentItem = await transaction.objectStore(parent.type).get(parent.key);
     if (isNull(parentItem)) {
       throw new NotFoundError(parent.type, parent.key);
@@ -160,18 +160,21 @@ export abstract class IdbBaseWriteCommand<T extends NdbDocument> extends IdbBase
                                        keys: ValidKey[]): Promise<void> {
     let parentChanged = false;
     const fieldValue = parentItem[parent.field];
-    if (Array.isArray(fieldValue)) {
+    const isArrayExpected = this.schema.getConfig(parent.type).targets[parent.field].isArray;
+    if (isArrayExpected) {
       if (isNull(fieldValue)) {
         parentItem[parent.field] = keys;
         parentChanged = true;
-      } else {
+      } else if (Array.isArray(fieldValue)) {
         keys.forEach(itemKey => {
           const index = fieldValue.findIndex(key => key === itemKey);
           if (index < 0) {
-            parentItem[parent.field].push(itemKey);
+            fieldValue.push(itemKey);
             parentChanged = true;
           }
         });
+      } else {
+        throw new TypeMismatchError(parent.type, parent.field, true);
       }
     } else {
       if (keys.length > 1) {
@@ -181,6 +184,10 @@ export abstract class IdbBaseWriteCommand<T extends NdbDocument> extends IdbBase
       const newChildKey = keys.shift();
       if (fieldValue !== newChildKey) {
         if (!isNull(fieldValue)) {
+          if (Array.isArray(fieldValue)) {
+            throw new TypeMismatchError(parent.type, parent.field, false);
+          }
+
           const oldChildCursor = await transaction.objectStore(this._type).openCursor(fieldValue);
           if (oldChildCursor) {
             const oldChild = oldChildCursor.value;
